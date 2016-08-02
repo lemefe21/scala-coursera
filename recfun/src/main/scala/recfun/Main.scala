ba5027346575dcb319cc76c59ae2aa0c864f0939
Felipe Leme <lemefe21@gmail.com>
2016-08-02 20:28:26 -0300

    Week 1 - Exercicio 2: Parentheses Balancing

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,14 +14,32 @@ object Main {
    * Exercise 1
    */
     def pascal(c: Int, r: Int): Int =
-      if(c == 0 || c == r) 1 
+      if(c == 0 || c == r || r == 0) 1 
       else pascal(c - 1, r - 1) + pascal(c, r - 1) 
 
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def verific(chars: List[Char], numOpens: Int): Boolean = {
+        //chars.isEmpty: Boolean returns whether a list is empty
+        if(chars.isEmpty){
+          numOpens == 0
+        } else {
+          //chars.head: Char returns the first element of the list
+          var headChar = chars.head
+          var countOpens =
+            if(headChar == '(') numOpens + 1
+            else if(headChar == ')') numOpens - 1
+            else numOpens
+            //chars.tail: List[Char] returns the list without the first element
+          if(countOpens >= 0) verific(chars.tail, countOpens)
+          else false
+        }
+      }
+      verific(chars, 0)        
+    }
   
   /**
    * Exercise 3
